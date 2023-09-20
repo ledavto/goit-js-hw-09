ab9208b0ddd70d2b9ce30237ee0b837f20dd9052
ledavto <ser_ega@ukr.net>
2023-09-20 13:41:56 +0300

    hw1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+let timerId = null;
+const elBody = document.body;
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+startBtn.addEventListener(
+  'click',
+  () =>
+    (timerId = setInterval(() => {
+      elBody.style.backgroundColor = getRandomHexColor();
+    }, 1000))
+);
+
+stopBtn.addEventListener('click', () => clearInterval(timerId));
+
+//console.log(saveBtn);
