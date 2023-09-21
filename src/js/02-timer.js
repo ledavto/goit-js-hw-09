6fb7a6d974f38a19af134437fed2c81ecb419dc6
ledavto <ser_ega@ukr.net>
2023-09-21 23:10:26 +0300

    update hm2

---
+++ src/js/02-timer.js
@@ -0,0 +1,15 @@
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(selector, options);
