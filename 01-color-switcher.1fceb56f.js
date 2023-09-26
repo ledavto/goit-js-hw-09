7f907ac1cc26a2e1bc7ebad5ea4a073fe771bb96
ledavto <101856417+ledavto@users.noreply.github.com>
2023-09-26 11:03:06 +0000

    Deploying to gh-pages from @ ledavto/goit-js-hw-09@39b86b7525fa254e13e0f25278eec9017edbde66 🚀

--- 01-color-switcher.1fceb56f.js
+++
@@ -1,2 +0,0 @@
-let t=null;const e=document.body,o=document.querySelector("button[data-start]"),a=document.querySelector("button[data-stop]");o.addEventListener("click",(()=>t=setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3))),a.addEventListener("click",(()=>clearInterval(t)));
-//# sourceMappingURL=01-color-switcher.1fceb56f.js.map
